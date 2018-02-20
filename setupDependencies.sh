8feb8ac2ba7a32d9f7ef966b400c03f24bd0a4d1
Ray Dennis <raydennis@outlook.com>
2018-02-20 09:32:51 -0700

    moved all dependencies to submodules and made setupDependencies.sh which creates the appropriate symbolic links

---
+++ setupDependencies.sh
@@ -0,0 +1,8 @@
+h -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ln -s /mnt/c/Users/Ray/GitHub/dotFiles/powerlevel9k ~/.oh-my-zsh/themes
+ln -s /mnt/c/Users/Ray/GitHub/dotFiles/zsh-autosuggestions ~/.oh-my-zsh/
+ln -s /mnt/c/Users/Ray/GitHub/dotFiles/zsh-syntax-highlighting ~/.oh-my-zsh/
+ln -s /mnt/c/Users/Ray/GitHub/dotFiles/z/z.sh /home/ray/z.sh
+ln -s /mnt/c/Users/Ray/GitHub/dotFiles/.vimrc ~/.vimrc
+ln -s /mnt/c/Users/Ray/GitHub/dotFiles/.zshrc ~/.zshrc
+ln -s /mnt/c/Users/Ray/GitHub/dotFiles/vim-monokai/colors/monokai.vim ~/.vim/colors/monokai.vim
