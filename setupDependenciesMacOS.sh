290e4765cc4a2908828873eea6643d1a30bf66e0
Ray Dennis <rdennis@it-d0p8f8j9.unm.edu>
2018-05-04 12:27:40 -0600

    updated vscode

---
+++ setupDependenciesMacOS.sh
@@ -0,0 +1,29 @@
+# get oh-my-zsh installed
+h -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# link the dotfiles
+ln -s ~/Documents/GitHub/dotFiles/powerlevel9k ~/.oh-my-zsh/themes
+ln -s ~/Documents/GitHub/dotFiles/zsh-autosuggestions ~/.oh-my-zsh/
+ln -s ~/Documents/GitHub/dotFiles/zsh-syntax-highlighting ~/.oh-my-zsh/
+ln -s ~/Documents/GitHub/dotFiles/z/z.sh ~/z.sh
+ln -s ~/Documents/GitHub/dotFiles/.vimrc ~/.vimrc
+ln -s ~/Documents/GitHub/dotFiles/.zshrc ~/.zshrc
+ln -s ~/Documents/GitHub/dotFiles/vim-monokai/colors/monokai.vim ~/.vim/colors/monokai.vim
+
+# setup dependencies for vscode
+# on Mac or Linux (WSL = ?) type: 
+# code --list-extensions | xargs -L 1 echo code --install-extension
+# to get the following list
+code --install-extension bbenoist.vagrant
+code --install-extension eamodio.gitlens
+code --install-extension ms-vscode.PowerShell
+code --install-extension ms-vsts.team
+code --install-extension robertohuertasm.vscode-icons
+code --install-extension ryu1kn.partial-diff
+code --install-extension vscodevim.vim
+code --install-extension waderyan.gitblame
+code --install-extension yzhang.dictionary-completion
+code --install-extension yzhang.markdown-all-in-one
+# now link to the user settings.json
+ln -s ~/Documents/GitHub/dotFiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+
---
+++ setupDependenciesWSL.sh
@@ -0,0 +1,8 @@
+h -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ln -s /Users/rdennis/GitHub/dotFiles/powerlevel9k ~/.oh-my-zsh/themes
+ln -s /Users/rdennis/GitHub/dotFiles/zsh-autosuggestions ~/.oh-my-zsh/
+ln -s /Users/rdennis/GitHub/dotFiles/zsh-syntax-highlighting ~/.oh-my-zsh/
+ln -s /Users/rdennis/GitHub/dotFiles/z/z.sh /home/ray/z.sh
+ln -s /Users/rdennis/GitHub/dotFiles/.vimrc ~/.vimrc
+ln -s /Users/rdennis/GitHub/dotFiles/.zshrc ~/.zshrc
+ln -s /Users/rdennis/GitHub/dotFiles/vim-monokai/colors/monokai.vim ~/.vim/colors/monokai.vim
