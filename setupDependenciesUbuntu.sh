eb69dd544bc57e1d54346ec7c02806ddd0347654
Ray Dennis <raydennis@outlook.com>
2018-07-02 11:08:06 -0600

    added nerdfont install to setupDependenciesUbuntu.sh

--- setupDependenciesUbuntu.sh
+++ setupDependenciesUbuntu.sh
@@ -4,6 +4,10 @@ sudo apt-get install zsh -y
 # get oh-my-zsh installed
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# Install nerdfont
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+
 # install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
 vim +BundleInstall +qall
