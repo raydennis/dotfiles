6221818d9380077f8d9fd9ba1a836795edf0c93c
Ray Dennis <raydennis@outlook.com>
2018-07-23 20:40:14 -0600

    Modified setupDependenciesMacOS.sh to fix typo in rm z.sh

--- OSs/setupDependenciesMacOS.sh
+++ OSs/setupDependenciesMacOS.sh
@@ -8,19 +8,32 @@ vim +BundleInstall +qall
 ~/.vim/bundle/fzf/install
 
 # link the dotfiles
-ln -s ~/Documents/GitHub/dotFiles/powerlevel9k ~/.oh-my-zsh/themes
-ln -s ~/Documents/GitHub/dotFiles/zsh-autosuggestions ~/.oh-my-zsh/
-ln -s ~/Documents/GitHub/dotFiles/zsh-syntax-highlighting ~/.oh-my-zsh/
-ln -s ~/Documents/GitHub/dotFiles/z/z.sh ~/z.sh
+rm ~/.oh-my-zsh/themes/powerlevel9k
+ln -s ~/Documents/GitHub/dotFiles/editors/vim/powerlevel9k ~/.oh-my-zsh/themes
+
+rm ~/.oh-my-zsh/zsh-*
+ln -s ~/Documents/GitHub/dotFiles/editors/vim/zsh-autosuggestions ~/.oh-my-zsh/
+ln -s ~/Documents/GitHub/dotFiles/editors/vim/zsh-syntax-highlighting ~/.oh-my-zsh/
+
+rm ~/z.sh
+ln -s ~/Documents/GitHub/dotFiles/shells/zsh/z/z.sh ~/z.sh
+
 rm ~/.vimrc
-ln -s ~/Documents/GitHub/dotFiles/.vimrc ~/.vimrc
+ln -s ~/Documents/GitHub/dotFiles/editors/vim/.vimrc ~/.vimrc
+
 rm ~/.zshrc
-ln -s ~/Documents/GitHub/dotFiles/.zshrc ~/.zshrc
-ln -s ~/Documents/GitHub/dotFiles/vim-monokai/colors/monokai.vim ~/.vim/colors/monokai.vim
-ln -s ~/Documents/GitHub/dotFiles/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/solarized.vim
+ln -s ~/Documents/GitHub/dotFiles/shells/zsh/.zshrc ~/.zshrc
+
+rm ~/.vim/colors/monokai.vim
+ln -s ~/Documents/GitHub/dotFiles/editors/vim/vim-monokai/colors/monokai.vim ~/.vim/colors/monokai.vim
+
+rm ~/.vim/colors/solarized.vim
+ln -s ~/Documents/GitHub/dotFiles/editors/vim/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/solarized.vim
+
 ln -s ~/Documents/GitHub/dotFiles/.gitconfig ~/.gitconfig
 ln -s ~/Documents/GitHub/dotFiles/.gitignore_global ~/.gitignore_global
-ln -s /mnt/c/Users/Ray/Documents/GitHub/dotFiles/en.utf-8.add ~/.vim/spell/en.utf-8.add
+rm ~/.vim/spell/en.utf-8.add
+ln -s /mnt/c/Users/Ray/Documents/GitHub/dotFiles/editors/vim/en.utf-8.add ~/.vim/spell/en.utf-8.add
 
 # setup dependencies for vscode
 # on Mac or Linux (WSL = ?) type: 
