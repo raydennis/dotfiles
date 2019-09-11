b4a1ea8928d2f582390efcdc4cf11961a2ccca43
Ray Dennis <raydennis@outlook.com>
2019-09-11 07:30:49 -0600

    updated vim aliases

--- shells/zsh/aliases.zsh
+++ shells/zsh/aliases.zsh
@@ -1,10 +1,12 @@
 alias tnc="ping -c 1 8.8.8.8 -t 1 | grep '0.0% \| 100.0%'"
 
 alias o="xdg-open"
+alias e="vim"
+alias q="exit"
 
 alias grd="git add . && git commit -am "fast update" && git push"
 alias gs="git status"
 
-alias ra="ranger"
+alias r="ranger"
 
 alias rgrep="ag -il 'first' | xargs ag -il 'second' | xargs ag -il 'third'"
