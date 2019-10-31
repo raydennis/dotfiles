07f9786467e221086eea164e558135bf87a24cec
Ray Dennis <raydennis@outlook.com>
2019-10-31 10:58:11 -0600

    added o, st, vir, t

--- shells/zsh/aliases.zsh
+++ shells/zsh/aliases.zsh
@@ -1,6 +1,14 @@
 alias tnc="ping -c 1 8.8.8.8 -t 1 | grep '0.0% \| 100.0%'"
 
-alias o="xdg-open"
+# TODO: Figure out how to load based on OS
+
+# *nix
+# alias o="xdg-open"
+
+# MacOS
+alias o="open"
+alias o.="open ."
+
 alias e="vim"
 alias q="exit"
 
@@ -12,3 +20,10 @@ alias r="ranger"
 alias rgrep="ag -il 'first' | xargs ag -il 'second' | xargs ag -il 'third'"
 
 alias c="clear"
+
+alias st="wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test10.zip"
+
+alias vir="vi -- *(D.om[1])"
+
+alias t="tree"
+
