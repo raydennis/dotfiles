d8166acb20cc8bd08e7b28f8db3876be7b47e605
Ray Dennis <raydennis@outlook.com>
2019-11-18 09:21:28 -0700

    cleaned up the dependencies for macos

--- os/setup-dependencies-macos.sh
+++ os/setup-dependencies-macos.sh
@@ -1,17 +1,5 @@
-# install zsh and oh-my-zsh
+# install zsh
 brew install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# link the dotfiles
-rm ~/.oh-my-zsh/themes/powerlevel9k
-ln -s ~/github/dotfiles/editors/vim/powerlevel9k ~/.oh-my-zsh/themes
-
-rm ~/.oh-my-zsh/zsh-*
-ln -s ~/github/dotfiles/shells/zsh/zsh-autosuggestions ~/.oh-my-zsh/
-ln -s ~/github/dotfiles/shells/zsh/zsh-syntax-highlighting ~/.oh-my-zsh/
-
-rm ~/.oh-my-zsh/custom/aliases.zsh
-ln -s ~/github/dotfiles/shells/zsh/aliases.zsh ~/.oh-my-zsh/custom
 
 rm ~/z.sh
 ln -s ~/github/dotfiles/shells/zsh/z/z.sh ~/z.sh
@@ -24,7 +12,6 @@ ln -s ~/github/dotfiles/editors/vim/coc-settings.json ~/.vim/coc-settings.json
 
 rm -rf ~/.vim/UltiSnips
 ln -s ~/github/dotfiles/editors/vim/ultisnips ~/.vim/plugged/UltiSnips
-
 
 rm ~/.zshrc
 ln -s ~/github/dotfiles/shells/zsh/.zshrc ~/.zshrc
@@ -42,9 +29,6 @@ ln -s ~/github/dotfiles/editors/vim/vim-colors-solarized/colors/solarized.vim ~/
 rm ~/.vim/colors/nord.vim
 ln -s ~/github/dotfiles/editors/vim/nord/nord-vim/colors/nord.vim ~/.vim/colors/nord.vim
 
-rm -rf ~/.vim/templates
-ln -s ~/github/dotfiles/editors/vim/templates ~/.vim/templates
-
 rm ~/.gitconfig
 ln -s ~/github/dotfiles/.gitconfig ~/.gitconfig
 
@@ -54,60 +38,57 @@ ln -s ~/github/dotfiles/.gitignore_global ~/.gitignore_global
 rm ~/.vim/spell/en.utf-8.add
 ln -s ~/github/dotfiles/editors/vim/en.utf-8.add ~/.vim/spell/en.utf-8.add
 
-# setup dependencies for vscode
+# # setup dependencies for vscode
 # on Mac or Linux (WSL = ?) type: 
 # code --list-extensions | xargs -L 1 echo code --install-extension
-code --install-extension 2gua.rainbow-brackets
-code --install-extension arcticicestudio.nord-visual-studio-code
-code --install-extension bbenoist.vagrant
-code --install-extension calebporzio.better-phpunit
-code --install-extension christian-kohler.path-intellisense
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension eamodio.gitlens
-code --install-extension esbenp.prettier-vscode
-code --install-extension humao.rest-client
-code --install-extension ikappas.composer
-code --install-extension johnpapa.winteriscoming
-code --install-extension ms-azure-devops.azure-pipelines
-code --install-extension ms-azuretools.vscode-apimanagement
-code --install-extension ms-azuretools.vscode-azureappservice
-code --install-extension ms-azuretools.vscode-azurefunctions
-code --install-extension ms-azuretools.vscode-azurestorage
-code --install-extension ms-azuretools.vscode-cosmosdb
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension ms-mssql.mssql
-code --install-extension ms-python.python
-code --install-extension ms-vscode.azure-account
-code --install-extension ms-vscode.azurecli
-code --install-extension ms-vscode.powershell
-code --install-extension ms-vscode.vscode-node-azure-pack
-code --install-extension ms-vscode.vscode-typescript-tslint-plugin
-code --install-extension ms-vsts.team
-code --install-extension msazurermtools.azurerm-vscode-tools
-code --install-extension PKief.material-icon-theme
-code --install-extension ryu1kn.partial-diff
-code --install-extension Sophisticode.php-formatter
-code --install-extension vsciot-vscode.azure-iot-toolkit
-code --install-extension vscodevim.vim
-code --install-extension waderyan.gitblame
-code --install-extension yzhang.dictionary-completion
-code --install-extension yzhang.markdown-all-in-one
+# code --install-extension 2gua.rainbow-brackets
+# code --install-extension arcticicestudio.nord-visual-studio-code
+# code --install-extension bbenoist.vagrant
+# code --install-extension calebporzio.better-phpunit
+# code --install-extension christian-kohler.path-intellisense
+# code --install-extension dbaeumer.vscode-eslint
+# code --install-extension eamodio.gitlens
+# code --install-extension esbenp.prettier-vscode
+# code --install-extension humao.rest-client
+# code --install-extension ikappas.composer
+# code --install-extension johnpapa.winteriscoming
+# code --install-extension ms-azure-devops.azure-pipelines
+# code --install-extension ms-azuretools.vscode-apimanagement
+# code --install-extension ms-azuretools.vscode-azureappservice
+# code --install-extension ms-azuretools.vscode-azurefunctions
+# code --install-extension ms-azuretools.vscode-azurestorage
+# code --install-extension ms-azuretools.vscode-cosmosdb
+# code --install-extension ms-azuretools.vscode-docker
+# code --install-extension ms-mssql.mssql
+# code --install-extension ms-python.python
+# code --install-extension ms-vscode.azure-account
+# code --install-extension ms-vscode.azurecli
+# code --install-extension ms-vscode.powershell
+# code --install-extension ms-vscode.vscode-node-azure-pack
+# code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+# code --install-extension ms-vsts.team
+# code --install-extension msazurermtools.azurerm-vscode-tools
+# code --install-extension PKief.material-icon-theme
+# code --install-extension ryu1kn.partial-diff
+# code --install-extension Sophisticode.php-formatter
+# code --install-extension vsciot-vscode.azure-iot-toolkit
+# code --install-extension vscodevim.vim
+# code --install-extension waderyan.gitblame
+# code --install-extension yzhang.dictionary-completion
+# code --install-extension yzhang.markdown-all-in-one
 
-# To enable key-repeating execute the following in your Terminal and restart VS Code.
-defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
-defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
-defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false
-defaults delete -g ApplePressAndHoldEnabled
+# # To enable key-repeating execute the following in your Terminal and restart VS Code.
+# defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+# defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
+# defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false
+# defaults delete -g ApplePressAndHoldEnabled
 
 # now link to the user settings.json
-ln -s ~/github/dotfiles/editors/vscode/settings.json ~/Library/Application\ Support/VSCodium/User/settings.json
-ln -s ~/github/dotfiles/editors/vscode/keybindings.json ~/Library/Application\ Support/VSCodium/User/keybindings.json
+# ln -s ~/github/dotfiles/editors/vscode/settings.json ~/Library/Application\ Support/VSCodium/User/settings.json
+# ln -s ~/github/dotfiles/editors/vscode/keybindings.json ~/Library/Application\ Support/VSCodium/User/keybindings.json
 
 # brew
-# brew install ansible cscope gdbm gettext git git-credential-manager glib gmp gnutls libevent libffi libtasn1 libunistring libyaml lua luajit macvim nettle nmap openconnect openssl p11-kit pcre pcre2 perl python readline ruby sqlite sshfs stoken terraform the_silver_searcher tree unbound vim xz z --HEAD universal-ctags/universal-ctags/universal-ctags aspell imgur-screenshot.sh terminal-notifier
-
-# sshfs
-# brew install osxfuse sshfs
+# brew install git git-credential-manager macvim nmap openconnect openssl python sshfs the_silver_searcher tree z aspell osxfuse 
 
 # emacs - spacemacs
 # brew cask install emacs
