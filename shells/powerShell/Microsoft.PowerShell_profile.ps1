ab82cd186bce5be794f16621ff95428dbb0ece98
Dennis <rsdenni@sandia.gov>
2020-01-16 10:34:32 -0700

    updated windows powershell profile
    added oh-my-posh and custom colors

--- shells/powerShell/Microsoft.PowerShell_profile.ps1
+++ shells/powerShell/Microsoft.PowerShell_profile.ps1
@@ -1,15 +1,28 @@
 ﻿Import-Module PSReadline
-Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-a4faccd\src\posh-git.psd1'
-Import-Module z
-$DC = "ent153cdc01.colleges.ad.unm.edu"
-function dcconnect
-{
-    runas /netonly /user:"colleges\raditsvc" powershell.exe
-}
-function start-mmcdsa
-{
-    mmc dsa.msc /server=$DC
-}
-
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+Import-Module z
+
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Agnoster
+$ThemeSettings.colors.AdminIconForegroundColor               = 'DarkYellow'
+$ThemeSettings.colors.CommandFailedIconForegroundColor       = 'DarkRed'
+$ThemeSettings.colors.DriveForegroundColor                   = 'Blue'
+$ThemeSettings.colors.DefaultColor                           = 'DarkGreen'
+$ThemeSettings.colors.GitForegroundColor                     = 'Black'
+$ThemeSettings.colors.GitLocalChangesColor                   = 'DarkYellow'
+$ThemeSettings.colors.GitNoLocalChangesAndAheadAndBehindColor= 'DarkRed'
+$ThemeSettings.colors.GitNoLocalChangesAndAheadColor         = 'DarkMagenta'
+$ThemeSettings.colors.GitNoLocalChangesAndBehindColor        = 'DarkRed'
+$ThemeSettings.colors.PromptBackgroundColor                  = 'Blue'
+$ThemeSettings.colors.PromptForegroundColor                  = 'White'
+$ThemeSettings.colors.PromptHighlightColor                   = 'Blue'
+$ThemeSettings.colors.PromptSymbolColor                      = 'White'
+$ThemeSettings.colors.SessionInfoBackgroundColor             = 'Black'
+$ThemeSettings.colors.SessionInfoForegroundColor             = 'White'
+$ThemeSettings.colors.VirtualEnvBackgroundColor              = 'Red'
+$ThemeSettings.colors.VirtualEnvForegroundColor              = 'White'
+$ThemeSettings.colors.WithBackgroundColor                    = 'DarkRed'
+$ThemeSettings.colors.WithForegroundColor                    = 'White'
