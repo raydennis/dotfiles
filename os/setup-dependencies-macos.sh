5054bdb3d9bd9e98bc17a5c7bf2531895667a683
Ray Dennis <raydennis@outlook.com>
2020-03-10 12:34:53 -0600

    Updated directory references from Github to Github/raydennis
    Repositories/Github/raydennis

--- os/setup-dependencies-macos.sh
+++ os/setup-dependencies-macos.sh
@@ -2,36 +2,36 @@
 brew install zsh z zsh-syntax-highlighting zsh-autosuggestions
 
 rm ~/z.sh
-ln -s ~/Github/dotfiles/shells/zsh/z/z.sh ~/z.sh
+ln -s ~/Repositories/Github/raydennis/dotfiles/shells/zsh/z/z.sh ~/z.sh
 
 rm ~/.vimrc
-ln -s ~/Github/dotfiles/editors/vim/.vimrc ~/.vimrc
+ln -s ~/Repositories/Github/raydennis/dotfiles/editors/vim/.vimrc ~/.vimrc
 
 rm ~/.vim/coc-settings.json
-ln -s ~/Github/dotfiles/editors/vim/coc-settings.json ~/.vim/coc-settings.json
+ln -s ~/Repositories/Github/raydennis/dotfiles/editors/vim/coc-settings.json ~/.vim/coc-settings.json
 
 rm -rf ~/.vim/UltiSnips
-ln -s ~/Github/dotfiles/editors/vim/ultisnips ~/.vim/UltiSnips
+ln -s ~/Repositories/Github/raydennis/dotfiles/editors/vim/ultisnips ~/.vim/UltiSnips
 
 rm ~/.zshrc
-ln -s ~/Github/dotfiles/shells/zsh/.zshrc ~/.zshrc
+ln -s ~/Repositories/Github/raydennis/dotfiles/shells/zsh/.zshrc ~/.zshrc
 
 rm -rf ~/notes
-ln -s ~/Github/notes ~/notes
+ln -s ~/Repositories/Github/raydennis/notes ~/notes
 
 rm ~/.vim/colors/monokai.vim
 mkdir ~/.vim/colors
-ln -s ~/Github/dotfiles/editors/vim/vim-monokai/colors/monokai.vim ~/.vim/colors/monokai.vim
+ln -s ~/Repositories/Github/raydennis/dotfiles/editors/vim/vim-monokai/colors/monokai.vim ~/.vim/colors/monokai.vim
 
 rm ~/.vim/colors/solarized.vim
-ln -s ~/Github/dotfiles/editors/vim/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/solarized.vim
+ln -s ~/Repositories/Github/raydennis/dotfiles/editors/vim/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/solarized.vim
 
 rm ~/.vim/colors/nord.vim
-ln -s ~/Github/dotfiles/editors/vim/nord/nord-vim/colors/nord.vim ~/.vim/colors/nord.vim
+ln -s ~/Repositories/Github/raydennis/dotfiles/editors/vim/nord/nord-vim/colors/nord.vim ~/.vim/colors/nord.vim
 
 # git
 rm ~/.gitconfig
-ln -s ~/Github/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/Repositories/Github/raydennis/dotfiles/.gitconfig ~/.gitconfig
 
 ## git-madge
 ### Dependencies
@@ -57,17 +57,17 @@ brew install imgcat
 # brew install jez/formulae/git-madge
 
 rm ~/.gitignore_global
-ln -s ~/Github/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/Repositories/Github/raydennis/dotfiles/.gitignore_global ~/.gitignore_global
 
 rm ~/.vim/spell/en.utf-8.add
-ln -s ~/Github/dotfiles/editors/vim/en.utf-8.add ~/.vim/spell/en.utf-8.add
+ln -s ~/Repositories/Github/raydennis/dotfiles/editors/vim/en.utf-8.add ~/.vim/spell/en.utf-8.add
 
 # brew
 # brew install git git-credential-manager macvim nmap openconnect openssl python sshfs the_silver_searcher tree z aspell osxfuse 
 
 # emacs - spacemacs
 # brew cask install emacs
-# git clone https://Github.com/syl20bnr/spacemacs ~/.emacs.d
+# git clone https://Repositories/Github/raydennis.com/syl20bnr/spacemacs ~/.emacs.d
 # rm -rf ~/.spacemacs
 # ln -s ~/gitHub/dotfiles/editors/emacs/.spacemacs ~/.spacemacs
 
@@ -78,7 +78,7 @@ brew install libcaca highlight atool lynx w3m elinks poppler transmission mediai
 sudo npm install ansimd -g
 rm -rf ~/.config/ranger
 ln -s ~/gitHub/dotfiles/shells/ranger ~/.config/ranger
-git clone https://Github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
+git clone https://Repositories/Github/raydennis.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
 
 # font
 brew tap homebrew/cask-fonts
@@ -87,5 +87,5 @@ brew cask install font-firacode-nerd-font
 # gitk theme
 rm -rf ~/.config/git/gitk
 mkdir -p ~/.config/git
-cp ~/Github/dotfiles/tools/gitk/gitk ~/.config/git/
+cp ~/Repositories/Github/raydennis/dotfiles/tools/gitk/gitk ~/.config/git/
 
