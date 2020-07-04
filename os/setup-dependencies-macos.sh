5a38ec5c2731fb006122268c93a20726d84db554
Ray Dennis <raydennis@outlook.com>
2020-07-04 15:46:28 -0600

    Added neovim

--- os/setup-dependencies-macos.sh
+++ os/setup-dependencies-macos.sh
@@ -10,6 +10,9 @@ ln -s ~/Repositories/Github/raydennis/dotfiles/editors/vim/.vimrc ~/.vimrc
 rm ~/.vim/coc-settings.json
 ln -s ~/Repositories/Github/raydennis/dotfiles/editors/vim/coc-settings.json ~/.vim/coc-settings.json
 
+rm -rf ~/.config/nvim
+ln -s ~/Repositories/Github/raydennis/dotfiles/editors/neovim ~/.config/nvim
+
 rm -rf ~/.vim/UltiSnips
 ln -s ~/Repositories/Github/raydennis/dotfiles/editors/vim/ultisnips ~/.vim/UltiSnips
 
@@ -28,6 +31,9 @@ ln -s ~/Repositories/Github/raydennis/dotfiles/editors/vim/vim-colors-solarized/
 
 rm ~/.vim/colors/nord.vim
 ln -s ~/Repositories/Github/raydennis/dotfiles/editors/vim/nord/nord-vim/colors/nord.vim ~/.vim/colors/nord.vim
+
+rm ~/.ideavimrc
+ln -s ~/Repositories/Github/raydennis/dotfiles/editors/IntellijIdea/.ideavimrc ~/.ideavimrc
 
 # git
 rm ~/.gitconfig
@@ -90,53 +96,53 @@ rm -rf ~/.config/git/gitk
 mkdir -p ~/.config/git
 cp ~/Repositories/Github/raydennis/dotfiles/tools/gitk/gitk ~/.config/git/
 
-# setup dependencies for vscode
-on Mac or Linux (WSL = ?) type: 
-code --list-extensions | xargs -L 1 echo code --install-extension
-code --install-extension 2gua.rainbow-brackets
-code --install-extension arcticicestudio.nord-visual-studio-code
-code --install-extension bbenoist.vagrant
-code --install-extension calebporzio.better-phpunit
-code --install-extension christian-kohler.path-intellisense
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension eamodio.gitlens
-code --install-extension esbenp.prettier-vscode
-code --install-extension humao.rest-client
-code --install-extension ikappas.composer
-code --install-extension johnpapa.winteriscoming
-code --install-extension ms-azure-devops.azure-pipelines
-code --install-extension ms-azuretools.vscode-apimanagement
-code --install-extension ms-azuretools.vscode-azureappservice
-code --install-extension ms-azuretools.vscode-azurefunctions
-code --install-extension ms-azuretools.vscode-azurestorage
-code --install-extension ms-azuretools.vscode-cosmosdb
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension ms-mssql.mssql
-code --install-extension ms-python.python
-code --install-extension ms-vscode.azure-account
-code --install-extension ms-vscode.azurecli
-code --install-extension ms-vscode.powershell
-code --install-extension ms-vscode.vscode-node-azure-pack
-code --install-extension ms-vscode.vscode-typescript-tslint-plugin
-code --install-extension ms-vsts.team
-code --install-extension msazurermtools.azurerm-vscode-tools
-code --install-extension PKief.material-icon-theme
-code --install-extension ryu1kn.partial-diff
-code --install-extension Sophisticode.php-formatter
-code --install-extension vsciot-vscode.azure-iot-toolkit
-code --install-extension vscodevim.vim
-code --install-extension waderyan.gitblame
-code --install-extension yzhang.dictionary-completion
-code --install-extension yzhang.markdown-all-in-one
+# # setup dependencies for vscode
+# on Mac or Linux (WSL = ?) type: 
+# code --list-extensions | xargs -L 1 echo code --install-extension
+# code --install-extension 2gua.rainbow-brackets
+# code --install-extension arcticicestudio.nord-visual-studio-code
+# code --install-extension bbenoist.vagrant
+# code --install-extension calebporzio.better-phpunit
+# code --install-extension christian-kohler.path-intellisense
+# code --install-extension dbaeumer.vscode-eslint
+# code --install-extension eamodio.gitlens
+# code --install-extension esbenp.prettier-vscode
+# code --install-extension humao.rest-client
+# code --install-extension ikappas.composer
+# code --install-extension johnpapa.winteriscoming
+# code --install-extension ms-azure-devops.azure-pipelines
+# code --install-extension ms-azuretools.vscode-apimanagement
+# code --install-extension ms-azuretools.vscode-azureappservice
+# code --install-extension ms-azuretools.vscode-azurefunctions
+# code --install-extension ms-azuretools.vscode-azurestorage
+# code --install-extension ms-azuretools.vscode-cosmosdb
+# code --install-extension ms-azuretools.vscode-docker
+# code --install-extension ms-mssql.mssql
+# code --install-extension ms-python.python
+# code --install-extension ms-vscode.azure-account
+# code --install-extension ms-vscode.azurecli
+# code --install-extension ms-vscode.powershell
+# code --install-extension ms-vscode.vscode-node-azure-pack
+# code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+# code --install-extension ms-vsts.team
+# code --install-extension msazurermtools.azurerm-vscode-tools
+# code --install-extension PKief.material-icon-theme
+# code --install-extension ryu1kn.partial-diff
+# code --install-extension Sophisticode.php-formatter
+# code --install-extension vsciot-vscode.azure-iot-toolkit
+# code --install-extension vscodevim.vim
+# code --install-extension waderyan.gitblame
+# code --install-extension yzhang.dictionary-completion
+# code --install-extension yzhang.markdown-all-in-one
 
 # To enable key-repeating execute the following in your Terminal and restart VS Code.
-defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
-defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
-defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false
-defaults delete -g ApplePressAndHoldEnabled
+# defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+# defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
+# defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false
+# defaults delete -g ApplePressAndHoldEnabled
 
 # now link to the user settings.json
-rm ~/Library/Application\ Support/VSCodium/User/settings.json
-rm ~/Library/Application\ Support/VSCodium/User/keybindings.json
-ln -s ~/Repositories/Github/raydennis/dotfiles/editors/vscode/settings.json ~/Library/Application\ Support/VSCodium/User/settings.json
-ln -s ~/Repositories/Github/raydennis/dotfiles/editors/vscode/keybindings.json ~/Library/Application\ Support/VSCodium/User/keybindings.json
+# rm ~/Library/Application\ Support/VSCodium/User/settings.json
+# rm ~/Library/Application\ Support/VSCodium/User/keybindings.json
+# ln -s ~/Repositories/Github/raydennis/dotfiles/editors/vscode/settings.json ~/Library/Application\ Support/VSCodium/User/settings.json
+# ln -s ~/Repositories/Github/raydennis/dotfiles/editors/vscode/keybindings.json ~/Library/Application\ Support/VSCodium/User/keybindings.json
