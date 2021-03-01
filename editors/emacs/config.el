e5ce55f21b3502bae531422913fb4d67e6c24848
Ray Dennis <raydennis@outlook.com>
2021-03-01 07:53:46 -0700

    Initial settings to org-mode and switch-window

--- editors/emacs/config.el
+++ editors/emacs/config.el
@@ -1,60 +1,46 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-(setq user-full-name "Ray Dennis"
-      user-mail-address "raydennis@outlook.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
 (setq
+ user-full-name "Ray Dennis"
+ user-mail-address "raydennis@outlook.com"
  doom-font (font-spec :family "FiraCode Nerd Font" :size 24 :weight 'regular)
  doom-variable-pitch-font (font-spec :family "FiraCode Nerd Font" :size 20)
  projectile-project-search-path '("~/Repositories/Azure")
- mac-command-modifier 'meta)
+ mac-command-modifier 'meta
+ scroll-margin 20
+ confirm-kill-emacs nil
+ auto-save-default t
+ make-backup-files t
+ doom-theme 'doom-one
+ display-line-numbers-type nil
+ org-directory "~/org/"
+ magit-ediff-dwim-show-on-hunks t
+)
 
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+; [Ace-Window] This changes the ace-window display to show a white letter with a red background. The box attribute adds some padding.
+(custom-set-faces!
+  '(aw-leading-char-face
+    :foreground "white" :background "red"
+    :weight bold :height 2.5 :box (:line-width 10 :color "red")))
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(map!
+ :leader :prefix "y" :desc "toggle word wrap" "w" #'toggle-word-wrap
+ :leader :desc "evil show regisers" "r" #'evil-show-registers
+ :leader :desc "show undow tree" "u" #'undo-tree-visualize
+ :leader :desc "show treemacs" "-" #'treemacs
+ :leader :prefix "z" :desc "correct previous word" "p" #'flyspell-check-previous-highlighted-word
+ :leader :prefix "z" :desc "correct previous word" "z" #'flyspell-auto-correct-word
+)
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type nil)
+(after! org-journal
+  (setq
+   org-journal-dir "~/Repositories/Azure/Sandia/wnotes/Journal"
+   org-journal-date-prefix "#+TITLE: "
+   org-journal-time-prefix "* "
+   org-journal-date-format "%a, %Y-%m-%d"
+   org-journal-file-format "%Y-%m-%d.org"
+        )
 
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+)
 
 (after! org-fancy-priorities
   (add-hook 'org-agenda-mode-hook 'org-fancy-priorities-mode)
