85ba60b4c9fa76ecc5dce2e9418e02a655f8196b
raydennis <raydennis@duck.com>
2021-12-29 10:25:17 -0700

    Made capitalization more consistent

--- os/setup-dependencies-wsl.sh
+++ os/setup-dependencies-wsl.sh
@@ -2,53 +2,53 @@
 sudo apt-get install zsh -y
 
 rm ~/.zshrc
-ln -s ~/Github/dotfiles/shells/zsh/.zshrc ~/.zshrc
+ln -s ~/repositories/github/raydennis/dotfiles/shells/zsh/.zshrc ~/.zshrc
 
-mkdir ~/.zsh/plugins
 
 rm -rf ~/.zsh/plugins/zsh-autosuggestions
-mkdir ~/.zsh/plugins/zsh-autosuggestions
-ln -s ~/Github/dotfiles/shells/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh ~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+mkdir -p ~/.zsh/plugins/zsh-autosuggestions
+ln -s ~/repositories/github/raydennis/dotfiles/shells/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh ~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 
 rm -rf ~/.zsh/plugins/zsh-syntax-highlighting
 mkdir ~/.zsh/plugins/zsh-syntax-highlighting
-ln -s ~/Github/dotfiles/shells/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+ln -s ~/repositories/github/raydennis/dotfiles/shells/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 rm ~/z.sh
-ln -s ~/Github/dotfiles/shells/zsh/z/z.sh ~/z.sh
+ln -s ~/repositories/github/raydennis/dotfiles/shells/zsh/z/z.sh ~/z.sh
+ln -s ~/re
 
 rm ~/.vimrc
-ln -s ~/Github/dotfiles/editors/vim/.vimrc ~/.vimrc
+ln -s ~/repositories/github/raydennis/dotfiles/editors/vim/.vimrc ~/.vimrc
 
 rm ~/.vim/coc-settings.json
-ln -s ~/Github/dotfiles/editors/vim/coc-settings.json ~/.vim/coc-settings.json
+ln -s ~/repositories/github/raydennis/dotfiles/editors/vim/coc-settings.json ~/.vim/coc-settings.json
 
 rm -rf ~/.vim/UltiSnips
-ln -s ~/Github/dotfiles/editors/vim/ultisnips ~/.vim/UltiSnips
+ln -s ~/repositories/github/raydennis/dotfiles/editors/vim/ultisnips ~/.vim/UltiSnips
 
 rm ~/.vim/colors/monokai.vim
 mkdir ~/.vim/colors
-ln -s ~/Github/dotfiles/editors/vim/vim-monokai/colors/monokai.vim ~/.vim/colors/monokai.vim
+ln -s ~/repositories/github/raydennis/dotfiles/editors/vim/vim-monokai/colors/monokai.vim ~/.vim/colors/monokai.vim
 
 rm ~/.vim/colors/solarized.vim
-ln -s ~/Github/dotfiles/editors/vim/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/solarized.vim
+ln -s ~/repositories/github/raydennis/dotfiles/editors/vim/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/solarized.vim
 
 rm ~/.vim/colors/nord.vim
-ln -s ~/Github/dotfiles/editors/vim/nord/nord-vim/colors/nord.vim ~/.vim/colors/nord.vim
+ln -s ~/repositories/github/raydennis/dotfiles/editors/vim/nord/nord-vim/colors/nord.vim ~/.vim/colors/nord.vim
 
 rm ~/.gitconfig
-ln -s ~/Github/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/repositories/github/raydennis/dotfiles/.gitconfig ~/.gitconfig
 
 rm ~/.gitignore_global
-ln -s ~/Github/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/repositories/github/raydennis/dotfiles/.gitignore_global ~/.gitignore_global
 
 rm ~/.vim/spell/en.utf-8.add
-ln -s ~/Github/dotfiles/editors/vim/en.utf-8.add ~/.vim/spell/en.utf-8.add
+ln -s ~/repositories/github/raydennis/dotfiles/editors/vim/en.utf-8.add ~/.vim/spell/en.utf-8.add
 
 # ranger
 sudo apt install ranger
 rm -rf ~/.config/ranger
-ln -s ~/Github/dotfiles/shells/ranger ~/.config/ranger
+ln -s ~/repositories/github/raydennis/dotfiles/shells/ranger ~/.config/ranger
 git clone https://Github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
 
