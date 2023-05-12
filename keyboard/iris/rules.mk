caf97025ef5c8b75854d5796c18d9d16e7080ef8
Ray Dennis <ray.dennis@intel.com>
2023-05-12 10:26:07 -0600

    Updated iris keymap for rev7

--- keyboard/iris/rules.mk
+++ keyboard/iris/rules.mk
@@ -1,5 +1,5 @@
-VIA_ENABLE = no
-ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-POINTING_DEVICE_ENABLE = yes
-
-SRC += "encoder_mouse.c"
+VIA_ENABLE = yes
+# ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+# POINTING_DEVICE_ENABLE = yes
+# SRC += "encoder_mouse.c"
+AUTO_SHIFT_ENABLE = yes
